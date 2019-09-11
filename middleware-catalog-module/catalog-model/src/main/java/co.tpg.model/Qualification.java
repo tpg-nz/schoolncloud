3ab6e177b80597f936edc7bb7b9c2ceecbabb21d
Honggang <xhg7@hotmail.com>
2019-09-11 19:04:23 +1200

    Added QualificationFunction.java, QualificationRequest.java, QualificationResponse.java

---
+++ middleware-catalog-module/catalog-model/src/main/java/co.tpg.model/Qualification.java
@@ -0,0 +1,36 @@
+package co.tpg.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Model class to represent a Qualification
+ * @author James
+ * @since 2019-09-11
+ */
+
+@Builder
+@Getter
+@Setter
+@ToString
+
+public class Qualification extends AbstractModel<Stirng> {
+    private String id;
+    private String name;
+    private String hyperlink;
+
+    public Qualification(String id, String name, String hyperlink) {
+        this.id = id;
+        this.name = name;
+        this.hyperlink = hyperlink;
+    }
+
+    public String toString() {
+        return "Qualification{" +
+                "id='" + this.id + "'" + ", name='" +
+                this.name + "'" + ", hyperlink='" + this.hyperlink +
+                "'" + "}";
+    }
+}
---
+++ middleware-catalog-module/catalog-model/src/main/java/co/tpg/response/QualificationResponse.java
@@ -0,0 +1,12 @@
+package co.tpg.response;
+
+import co.tpg.model.Qualification;
+
+/**
+ * Response class for Qualification model.
+ * @author James
+ * @since 2019-09-11
+ */
+public class QualificationResponse extends AbstractResponse<Qualification> {
+    
+}

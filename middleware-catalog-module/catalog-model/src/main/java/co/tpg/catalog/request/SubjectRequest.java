e45a763028d837f43488fef59cb4324301114926
maxx371 <gorbatov.max@gmail.com>
2019-10-07 16:16:27 +1300

    added request and response

---
+++ middleware-catalog-module/catalog-model/src/main/java/co/tpg/catalog/request/SubjectRequest.java
@@ -0,0 +1,14 @@
+package co.tpg.catalog.request;
+
+import co.tpg.catalog.model.Subject;
+
+/**
+ * Request class for Subject entity
+ * @author maxx
+ * @since 03-10-2019
+ */
+
+public class SubjectRequest extends AbstractRequest<Subject>{
+    public SubjectRequest() {
+    }
+}
---
+++ middleware-catalog-module/catalog-model/src/main/java/co/tpg/catalog/response/SubjectResponse.java
@@ -0,0 +1,12 @@
+package co.tpg.catalog.response;
+
+import co.tpg.catalog.model.Subject;
+
+/**
+ * Response class for Subject entity
+ * @author maxx
+ * @since 2019-10-03
+ */
+
+public class SubjectResponse extends AbstractResponse<Subject>{
+}
